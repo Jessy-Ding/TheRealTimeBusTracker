35afb51f801f2236ba10030bd883e056ba8af5f0
Jessy-Ding <89423283+Jessy-Ding@users.noreply.github.com>
2023-12-14 16:18:07 -0500

    Init commit

---
+++ mapanimation.js
@@ -0,0 +1,34 @@
+mapboxgl.accessToken = '--INSertYourAccessTokenHere--'; 
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.091542, 42.358862], // Boston, MA
+    zoom: 12
+});
+
+function updateMap(vehicles) {
+    vehicles.forEach(vehicle => {
+        const el = document.createElement('div');
+        el.className = 'marker';
+
+        console.log(`Creating marker at [${vehicle.attributes.longitude}, ${vehicle.attributes.latitude}]`); // See the bus coordinates in the console; here we use route1 as an example
+
+        new mapboxgl.Marker(el)
+            .setLngLat([vehicle.attributes.longitude, vehicle.attributes.latitude])
+            .addTo(map);
+    });
+}
+
+async function fetchVehiclePositions() {
+    try {
+        const response = await fetch('https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip'); // Decide which route you want to track here
+        const jsonResponse = await response.json();
+        updateMap(jsonResponse.data);
+    } catch (error) {
+        console.error('Error fetching vehicle positions:', error);
+    }
+}
+
+setInterval(fetchVehiclePositions, 10000); // Change your refresh rate here
+fetchVehiclePositions();
